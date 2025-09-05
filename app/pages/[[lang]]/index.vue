a24c431d3bbf358d28d230f31f3a9015955a1f78
Sandro Circi <sandro.circi@digitoolmedia.com>
2025-09-05 13:24:06 +0200

    fix(temp): prose components not being loaded

---
+++ app/pages/[[lang]]/index.vue
@@ -0,0 +1,48 @@
+<!-- TODO: remove this once https://github.com/nuxtlabs/docus/pull/1174 gets merged -->
+<script setup lang="ts">
+import type { Collections } from '@nuxt/content'
+
+const route = useRoute()
+const { locale, isEnabled } = useDocusI18n()
+
+// Dynamic collection name based on i18n status
+const collectionName = computed(() => isEnabled.value ? `landing_${locale.value}` : 'landing')
+
+const { data: page } = await useAsyncData(collectionName.value, () => queryCollection(collectionName.value as keyof Collections).path(route.path).first())
+if (!page.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+}
+
+// Reconsider it once this is implemented: https://github.com/nuxt/content/issues/3419
+const prose = page.value.meta?.prose as boolean
+const title = page.value.seo?.title || page.value.title
+const description = page.value.seo?.description || page.value.description
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+})
+
+if (page.value?.seo?.ogImage) {
+  useSeoMeta({
+    ogImage: page.value.seo.ogImage,
+    twitterImage: page.value.seo.ogImage,
+  })
+}
+else {
+  defineOgImageComponent('Landing', {
+    title,
+    description,
+  })
+}
+</script>
+
+<template>
+  <ContentRenderer
+    v-if="page"
+    :value="page"
+    :prose="prose"
+  />
+</template>
