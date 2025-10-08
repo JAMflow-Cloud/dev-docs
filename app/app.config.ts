d4275f8c3bfc279b8772f023465fdd30a9ade41e
Sandro Circi <sandro.circi@digitoolmedia.com>
2025-10-08 17:33:26 +0200

    style: ui components

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,4 +2,18 @@ export default defineAppConfig({
   header: {
     title: 'JAMflow Docs',
   },
+  ui: {
+    pageHero: {
+      slots: {
+        title: 'font-semibold sm:text-6xl',
+        container: '!pb-0',
+      },
+    },
+    pageCard: {
+      slots: {
+        container: 'lg:flex min-w-0',
+        wrapper: 'flex-none',
+      },
+    },
+  },
 })
